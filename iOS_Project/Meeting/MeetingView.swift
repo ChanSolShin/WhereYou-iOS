57007e3c8e47cf9212452e0c7e5c5667106192d3
Shin Chan Sol <leeh4743@naver.com>
2024-10-28 01:13:09 +0900

    Fix. MeetingView
    멤버이름 버튼에서 멤버이름이 파이어베이스에서 정상적으로 불러오도록 수정

--- iOS_Project/Meeting/MeetingView.swift
+++ iOS_Project/Meeting/MeetingView.swift
@@ -21,14 +21,21 @@ struct MeetingView: View {
             Text(title + meeting.meetingAddress)
                 .font(.headline)
             
-            MeetingMapView(meeting: MeetingModel(title: meeting.title, date: meeting.date, meetingAddress: meeting.meetingAddress, meetingLocation: meeting.meetingLocation, meetingMemberIDs: meeting.meetingMemberIDs, meetingMasterID: meeting.meetingMasterID))
-                .frame(height: 300)
-
+            MeetingMapView(
+                meeting: MeetingModel(
+                    title: meeting.title,
+                    date: meeting.date,
+                    meetingAddress: meeting.meetingAddress,
+                    meetingLocation: meeting.meetingLocation,
+                    meetingMemberIDs: meeting.meetingMemberIDs,
+                    meetingMasterID: meeting.meetingMasterID
+                )
+            )
+            .frame(height: 300)
 
             ForEach(meeting.meetingMemberIDs, id: \.self) { memberID in
-                Button(action: {
-                }) {
-                    Text(meetingViewModel.meetingMemberNames[memberID] ?? memberID) // 이름으로 표시
+                Button(action: {}) {
+                    Text(meetingViewModel.meetingMemberNames[memberID] ?? "멤버 불러오는 중 ...")
                         .padding()
                         .background(Color.blue)
                         .foregroundColor(.white)
@@ -36,6 +43,16 @@ struct MeetingView: View {
                 }
                 .padding(.vertical, 2)
             }
+        }
+        .onAppear {
+            meetingViewModel.selectMeeting(meeting: MeetingModel(
+                title: meeting.title,
+                date: meeting.date,
+                meetingAddress: meeting.meetingAddress,
+                meetingLocation: meeting.meetingLocation,
+                meetingMemberIDs: meeting.meetingMemberIDs,
+                meetingMasterID: meeting.meetingMasterID
+            ))
         }
         .navigationTitle(meeting.title)
         .navigationBarTitleDisplayMode(.inline)
