45d8da3e941c92142b68e81180bf5fbc5c9be3c9
Shin Chan Sol <leeh4743@naver.com>
2024-10-28 01:13:09 +0900

    Fix. MeetingView
    멤버이름 버튼에서 멤버이름이 파이어베이스에서 정상적으로 불러오도록 수정

--- iOS_Project/Meeting/MeetingViewModel.swift
+++ iOS_Project/Meeting/MeetingViewModel.swift
@@ -8,36 +8,30 @@
 import Foundation
 import CoreLocation
 import FirebaseFirestore
+import Combine
 
 class MeetingViewModel: ObservableObject {
     @Published var meetingLocation: CLLocationCoordinate2D?
-    
-    @Published var meetingMemberNames: [String: String] = [:] // [userID: userName]
-    
+    @Published var meetingMemberNames: [String: String] = [:]
+
     func selectMeeting(meeting: MeetingModel) {
         self.meetingLocation = meeting.meetingLocation
-        
-        
+
         for memberID in meeting.meetingMemberIDs {
             fetchUserName(byID: memberID) { [weak self] name in
                 DispatchQueue.main.async {
-                    self?.meetingMemberNames[memberID] = name // UI 업데이트
+                    self?.meetingMemberNames[memberID] = name
                 }
             }
         }
     }
-    
+
     private func fetchUserName(byID userID: String, completion: @escaping (String) -> Void) {
         let db = Firestore.firestore()
-        let usersCollection = db.collection("users")
-        
-        usersCollection.document(userID).getDocument { (document, error) in
-            if let document = document, document.exists {
-                if let userName = document.data()?["name"] as? String {
-                    completion(userName)
-                } else {
-                    completion("Unknown")
-                }
+        db.collection("users").document(userID).getDocument { (document, error) in
+            if let document = document, document.exists,
+               let userName = document.data()?["name"] as? String {
+                completion(userName)
             } else {
                 completion("Unknown")
             }
