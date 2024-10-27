5f244599b9f068b1948c227493271f10f7e5319b
Shin Chan Sol <leeh4743@naver.com>
2024-10-28 02:21:21 +0900

    Design. MeetingView
    MeetingView의 지도 디자인개선, 버튼을 클릭시 Map을 업데이트 하는 가이드라인 제공

--- iOS_Project/Meeting/MeetingView.swift
+++ iOS_Project/Meeting/MeetingView.swift
@@ -13,13 +13,13 @@ import FirebaseFirestore
 struct MeetingView: View {
     var meeting: MeetingListModel
     @ObservedObject var meetingViewModel: MeetingViewModel
-
-    var title: String = "모임장소: "
+    @State private var title: String = "모임장소" //
     
     var body: some View {
         VStack {
-            Text(title + meeting.meetingAddress)
-                .font(.headline)
+            
+            Text(title)
+                .font(.title2)
             
             MeetingMapView(
                 meeting: MeetingModel(
@@ -32,17 +32,42 @@ struct MeetingView: View {
                 )
             )
             .frame(height: 300)
-
-            ForEach(meeting.meetingMemberIDs, id: \.self) { memberID in
-                Button(action: {}) {
-                    Text(meetingViewModel.meetingMemberNames[memberID] ?? "멤버 불러오는 중 ...")
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
-                }
-                .padding(.vertical, 2)
+            
+            Button(action: {
+                title = "모임장소"
+                // 모임장소의 위치를 Map에 업데이트
+            }) {
+                Text("모임장소")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
             }
+            // 멤버 버튼을 가로 3개씩 배치
+            ForEach(0..<meeting.meetingMemberIDs.count/3 + (meeting.meetingMemberIDs.count % 3 > 0 ? 1 : 0), id: \.self) { rowIndex in
+                HStack {
+                    // 각 행에서 3개의 버튼 생성
+                    ForEach(0..<3) { columnIndex in
+                        let index = rowIndex * 3 + columnIndex
+                        if index < meeting.meetingMemberIDs.count {
+                            let memberID = meeting.meetingMemberIDs[index]
+                            Button(action: {
+                                title = (meetingViewModel.meetingMemberNames[memberID] ?? "멤버") + "의 위치"
+                                // 버튼에 해당하는 사람의 현재위치정보를 Map에 업데이트 해야함
+                                
+                            }) {
+                                Text(meetingViewModel.meetingMemberNames[memberID] ?? "멤버 불러오는 중 ...")
+                                    .padding()
+                                    .background(Color.blue)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(10)
+                            }
+                            .padding(.vertical, 2)
+                        }
+                    }
+                }
+            }
+            
         }
         .onAppear {
             meetingViewModel.selectMeeting(meeting: MeetingModel(
