897d93c3ee573f2c456367c93eca68434af5a876
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-11-01 00:13:10 +0900

    [Feat] 이메일 찾기 기능

---
+++ iOS_Project/FindAuth/FindEmailView.swift
@@ -0,0 +1,104 @@
+//
+//  FindEmailView.swift
+//  iOS_Project
+//
+//  Created by CHOI on 10/31/24.
+//
+import SwiftUI
+
+struct FindEmailView: View {
+    @StateObject private var viewModel = FindEmailViewModel()
+    @State private var showAlert = false
+    
+    var body: some View {
+        ScrollView{
+            VStack {
+                Text("MyApp")
+                    .font(.largeTitle)
+                    .padding(.top, 20)
+                    .padding(.bottom, 60)
+                    .fontWeight(.bold)
+                
+                
+                VStack {
+                    HStack{
+                        Image(systemName: "person.fill")
+                            .foregroundColor(.gray)
+                            .imageScale(.small)
+                        TextField("이름을 입력하세요", text: $viewModel.name)
+                        
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .padding(.bottom,10)
+                    
+                    HStack{
+                        Image(systemName: "calendar")
+                            .foregroundColor(.gray)
+                            .imageScale(.small)
+                        TextField("생년월일(8자리)", text: $viewModel.birthday)
+                            .keyboardType(.numberPad)
+                            .onChange(of: viewModel.birthday) { newValue in
+                                if newValue.count > 8 {
+                                    viewModel.birthday = String(newValue.prefix(8))
+                                }
+                            }
+                            .font(.system(size: 16))
+                        
+                    }.padding()
+                        .background(Color.gray.opacity(0.2))
+                        .cornerRadius(8)
+                        .padding(.horizontal)
+                        .padding(.bottom,10)
+                    
+                    HStack{
+                        Image(systemName: "phone")
+                            .foregroundColor(.gray)
+                            .imageScale(.small)
+                        TextField("전화번호를 입력하세요 ( - 없이 )", text:$viewModel.phoneNumber)
+                            .keyboardType(.numberPad)
+                    }
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+                    .padding(.horizontal)
+                    .padding(.bottom,20)
+                    
+                    Button(action: {
+                        viewModel.findEmail { success in
+                            showAlert = !success
+                        }
+                    }) {
+                        Text("이메일 찾기")
+                            .frame(width: 200, height: 50)
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    .padding(.bottom,10)
+                    .alert(isPresented: $showAlert) {
+                        Alert(title: Text("알림"), message: Text(viewModel.errorMessage ?? "일치하는 계정이 없습니다."))
+                    }
+                    
+                    if let foundEmail = viewModel.foundEmail {
+                        Text("사용 중인 이메일: \(foundEmail)")
+                            .foregroundColor(.blue)
+                            .padding()
+                    }
+                    Spacer()
+                }
+            }
+            .navigationTitle("이메일 찾기")
+        }
+        
+    }
+}
+// 프리뷰용 코드
+struct FindEmailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FindEmailView()
+    }
+}
+
