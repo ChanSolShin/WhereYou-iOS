98a8128d351f467bbbd666050c9a95e79ede6d25
= <leeh4743@naver.com>
2024-11-05 03:51:04 +0900

    Feat. AddSelectedFriend
    친구 초대하는 기능 완성
    초대후 수락하면, 해당하는 모임의 멤버로 추가. 해당 meeting의 내용이 공유됨.
    추가된 멤버의 버튼이 자동으로 생성되는것 확인완료

--- iOS_Project/Meeting/MeetingViewModel.swift
+++ iOS_Project/Meeting/MeetingViewModel.swift
@@ -112,15 +112,40 @@ class MeetingViewModel: ObservableObject {
         }
     }
     
-    func acceptMeetingRequest(requestID: String, fromUserID: String) {
+    func acceptMeetingRequest(requestID: String, toUserID: String) {
         let db = Firestore.firestore()
-        db.collection("meetingRequests").document(requestID).updateData(["status": "accepted"]) { [weak self] error in
+        
+        // 요청 문서 가져오기
+        db.collection("meetingRequests").document(requestID).getDocument { [weak self] document, error in
+            guard let self = self else { return }
+            
             if let error = error {
-                print("Error accepting meeting request: \(error)")
+                print("Error fetching meeting request: \(error)")
+                return
+            }
+            
+            if let document = document, document.exists {
+                let meetingID = document.data()?["meetingID"] as? String ?? ""
+                // 수락 요청 상태 업데이트
+                self.updateMeetingRequestStatus(requestID: requestID, status: "accepted")
+                
+                // Meeting에 사용자 추가
+                self.addUserToMeeting(meetingID: meetingID, userID: toUserID)
+            }
+        }
+    }
+
+    private func addUserToMeeting(meetingID: String, userID: String) {
+        let db = Firestore.firestore()
+        
+        // meetings 컬렉션의 해당 문서 업데이트
+        db.collection("meetings").document(meetingID).updateData([
+            "meetingMembers": FieldValue.arrayUnion([userID])  // 사용자 ID 추가
+        ]) { error in
+            if let error = error {
+                print("Error adding user to meeting: \(error)")
             } else {
-                self?.deleteMeetingRequest(requestID: requestID)
-                // 요청 삭제 후 최신 요청 목록 불러오기
-                self?.fetchPendingMeetingRequests()
+                print("Successfully added user \(userID) to meeting with ID: \(meetingID)")
             }
         }
     }
