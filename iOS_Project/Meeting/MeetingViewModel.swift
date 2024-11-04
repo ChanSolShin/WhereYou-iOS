bb9fdbc711564ef4cff492b2defaaa32d438ce65
= <leeh4743@naver.com>
2024-11-04 11:45:22 +0900

    FIx. MeetingListView
    모임요청이 왔을때 온 요청수만큼 배지에 표시하는 기능이 동작하지않는 버그 수정

--- iOS_Project/Meeting/MeetingViewModel.swift
+++ iOS_Project/Meeting/MeetingViewModel.swift
@@ -17,10 +17,10 @@ class MeetingViewModel: ObservableObject {
     @Published var pendingMeetingRequests: [MeetingRequestModel] = []
     @Published var showAlert: Bool = false
     @Published var errorMessage: String?
-
+    
     func selectMeeting(meeting: MeetingModel) {
         self.meetingLocation = meeting.meetingLocation
-
+        
         for memberID in meeting.meetingMemberIDs {
             fetchUserName(byID: memberID) { [weak self] name in
                 DispatchQueue.main.async {
@@ -29,63 +29,105 @@ class MeetingViewModel: ObservableObject {
             }
         }
     }
-
+    
     private func fetchUserName(byID userID: String, completion: @escaping (String) -> Void) {
         let db = Firestore.firestore()
         db.collection("users").document(userID).getDocument { (document, error) in
-            if let document = document, document.exists,
-               let userName = document.data()?["name"] as? String {
+            if let error = error {
+                print("Error fetching user name: \(error)")
+                completion("Unknown")
+            } else if let document = document, document.exists,
+                      let userName = document.data()?["name"] as? String {
                 completion(userName)
             } else {
                 completion("Unknown")
             }
         }
     }
-
+    
     // 친구들에게 모임 초대 요청 보내기
     func sendMeetingRequest(toUserIDs: [String], meetingName: String, fromUserID: String, fromUserName: String) {
         let db = Firestore.firestore()
         
         for userID in toUserIDs {
-            db.collection("meetingRequests")
+            let requestReference = db.collection("meetingRequests")
                 .whereField("fromUserID", isEqualTo: fromUserID)
                 .whereField("toUserID", isEqualTo: userID)
                 .whereField("meetingName", isEqualTo: meetingName)
                 .whereField("status", isEqualTo: "pending")
-                .getDocuments { (snapshot, error) in
-                    if let error = error {
-                        print("Error checking existing requests: \(error)")
-                        return
-                    }
+            
+            requestReference.getDocuments { (snapshot, error) in
+                if let error = error {
+                    print("Error checking existing requests: \(error)")
+                    return
                 }
+                
+                // 요청이 존재하지 않는 경우 새로운 요청을 추가합니다.
+                if let documents = snapshot?.documents, documents.isEmpty {
+                    self.createMeetingRequest(toUserID: userID, meetingName: meetingName, fromUserID: fromUserID, fromUserName: fromUserName)
+                } else {
+                    print("Request already exists for \(userID)")
+                }
+            }
         }
     }
-    // 수락 및 거절 요청 처리
+    
+    private func createMeetingRequest(toUserID: String, meetingName: String, fromUserID: String, fromUserName: String) {
+        let db = Firestore.firestore()
+        let newRequest: [String: Any] = [
+            "fromUserID": fromUserID,
+            "fromUserName": fromUserName,
+            "toUserID": toUserID,
+            "meetingName": meetingName,
+            "status": "pending"
+        ]
+        
+        db.collection("meetingRequests").addDocument(data: newRequest) { error in
+            if let error = error {
+                print("Error creating meeting request: \(error)")
+            } else {
+                print("Successfully created meeting request to \(toUserID)")
+            }
+        }
+    }
+    
     func acceptMeetingRequest(requestID: String, fromUserID: String) {
         let db = Firestore.firestore()
-        // 요청을 수락하고 필요한 로직 추가
-        db.collection("meetingRequests").document(requestID).updateData(["status": "accepted"]) { error in
+        db.collection("meetingRequests").document(requestID).updateData(["status": "accepted"]) { [weak self] error in
             if let error = error {
                 print("Error accepting meeting request: \(error)")
             } else {
-                // 수락 후 추가 로직 구현
+                self?.deleteMeetingRequest(requestID: requestID)
+                // 요청 삭제 후 최신 요청 목록 불러오기
+                self?.fetchPendingMeetingRequests()
+            }
+        }
+    }
+    
+    func rejectMeetingRequest(requestID: String) {
+        let db = Firestore.firestore()
+        db.collection("meetingRequests").document(requestID).updateData(["status": "rejected"]) { [weak self] error in
+            if let error = error {
+                print("Error rejecting meeting request: \(error)")
+            } else {
+                self?.deleteMeetingRequest(requestID: requestID)
+                // 요청 삭제 후 최신 요청 목록 불러오기
+                self?.fetchPendingMeetingRequests()
+            }
+        }
+    }
+    
+    private func updateMeetingRequestStatus(requestID: String, status: String) {
+        let db = Firestore.firestore()
+        db.collection("meetingRequests").document(requestID).updateData(["status": status]) { error in
+            if let error = error {
+                print("Error updating meeting request: \(error)")
+            } else {
                 self.deleteMeetingRequest(requestID: requestID) // 요청 삭제
             }
         }
     }
-
-    func rejectMeetingRequest(requestID: String) {
-        let db = Firestore.firestore()
-        // 요청을 거절하고 필요한 로직 추가
-        db.collection("meetingRequests").document(requestID).updateData(["status": "rejected"]) { error in
-            if let error = error {
-                print("Error rejecting meeting request: \(error)")
-            } else {
-                self.deleteMeetingRequest(requestID: requestID)
-            }
-        }
-    }
-
+    
     // 요청 삭제
     private func deleteMeetingRequest(requestID: String) {
         let db = Firestore.firestore()
@@ -100,30 +142,37 @@ class MeetingViewModel: ObservableObject {
             }
         }
     }
-
+    
+    private var listener: ListenerRegistration?
+    
     func fetchPendingMeetingRequests() {
         let db = Firestore.firestore()
         guard let currentUserID = Auth.auth().currentUser?.uid else { return }
-
-        db.collection("meetingRequests")
+        
+        // 기존 리스너 제거
+        listener?.remove()
+        
+        // 새로운 리스너 추가
+        listener = db.collection("meetingRequests")
             .whereField("toUserID", isEqualTo: currentUserID)
             .whereField("status", isEqualTo: "pending")
-            .getDocuments { (querySnapshot, error) in
+            .addSnapshotListener { [weak self] (querySnapshot, error) in
                 if let error = error {
                     print("Error fetching pending meeting requests: \(error)")
                 } else {
-                    self.pendingMeetingRequests = querySnapshot?.documents.compactMap { document in
-                        let data = document.data()
-                        return MeetingRequestModel(
-                            id: document.documentID,
-                            fromUserID: data["fromUserID"] as? String ?? "",
-                            fromUserName: data["fromUserName"] as? String ?? "",
-                            toUserID: data["toUserID"] as? String ?? "",
-                            meetingName: data["meetingName"] as? String ?? "",
-                            status: data["status"] as? String ?? ""
-                        )
-                    } ?? []
+                    DispatchQueue.main.async {
+                        self?.pendingMeetingRequests = querySnapshot?.documents.compactMap { document in
+                            let data = document.data()
+                            return MeetingRequestModel(
+                                id: document.documentID,
+                                fromUserID: data["fromUserID"] as? String ?? "",
+                                fromUserName: data["fromUserName"] as? String ?? "",
+                                toUserID: data["toUserID"] as? String ?? "",
+                                meetingName: data["meetingName"] as? String ?? "",
+                                status: data["status"] as? String ?? ""
+                            )
+                        } ?? []
+                    }
                 }
             }
-    }
-}
+    }}
