0b63275ed47fe7d47d5c151f40fc77b54582eff5
HunCY5 <90296157+HunCY5@users.noreply.github.com>
2024-11-05 04:51:44 +0900

    [Feat] 오류 신고 화면
    - Firestore report컬렉션에 저장

---
+++ iOS_Project/Profile/ReportView.swift
@@ -0,0 +1,70 @@
+//
+//  ReportView.swift
+//  iOS_Project
+//
+//  Created by CHOI on 11/5/24.
+//
+
+import SwiftUI
+
+struct ReportView: View {
+    @ObservedObject private var viewModel = ReportViewModel()
+    @Environment(\.presentationMode) var presentationMode
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 16) {
+            Text("오류 신고")
+                .font(.title)
+                .bold()
+                .padding(.top)
+            
+            Text("신고내용")
+                .font(.headline)
+                .foregroundColor(Color.black)
+            
+            TextEditor(text: $viewModel.reportContent)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color.gray, lineWidth: 1)
+                )
+                .background(Color.white)
+                .frame(height: 200)
+                .overlay(
+                    Text(viewModel.reportContent.isEmpty ? "ex) 신고내용을 입력해주세요." : "")
+                        .foregroundColor(.gray)
+                        .padding(.horizontal, 20)
+                        .padding(.vertical, 25),
+                    alignment: .topLeading
+                )
+            
+            Spacer()
+            
+            Button(action: {
+                viewModel.submitReport()
+            }) {
+                Text("신고하기")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+            
+            if let statusMessage = viewModel.submissionStatus {
+                Text(statusMessage)
+                    .foregroundColor(.gray)
+                    .padding(.top)
+            }
+        }
+        .padding()
+        
+    }
+}
+// 프리뷰용 코드
+struct ReportView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReportView()
+    }
+}
