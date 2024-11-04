257d9e4e8ccc07023aed798e7b119e4b72a52962
= <leeh4743@naver.com>
2024-11-05 03:59:24 +0900

    Feat. AddSelectedFirend
    이미 모임에 참여된 멤버를 초대시, 이미 해당모임에 참여됬다는 알림메시지

--- iOS_Project/Meeting/AddSelectedFriend.swift
+++ iOS_Project/Meeting/AddSelectedFriend.swift
@@ -17,7 +17,7 @@ struct AddSelectedFriend: View {
     @State private var showAlert: Bool = false
     @State private var alertMessage: String = ""
     @Environment(\.presentationMode) var presentationMode
-
+    
     var body: some View {
         NavigationView {
             ZStack(alignment: .bottom) {
@@ -68,7 +68,6 @@ struct AddSelectedFriend: View {
             selectedFriends.append(friend.id)
         }
     }
-    
     func sendMeetingInvitations() {
         guard let currentUserID = Auth.auth().currentUser?.uid,
               let currentUserName = friendListViewModel.currentUserName else { return }
@@ -77,79 +76,99 @@ struct AddSelectedFriend: View {
         var failedInvitations = [String]()
         let totalFriendsCount = selectedFriends.count
         var completedRequests = 0 // 요청 완료된 수
-    
+        
         for friendID in selectedFriends {
-            // 이미 보낸 요청이 있는지 확인
-            db.collection("meetingRequests")
-                .whereField("fromUserID", isEqualTo: currentUserID)
-                .whereField("toUserID", isEqualTo: friendID)
-                .whereField("meetingName", isEqualTo: meeting.title)
-                .getDocuments { snapshot, error in
-                    if let error = error {
-                        print("요청 확인 중 오류 발생: \(error)")
+            // 모임에 이미 참여하고 있는지 확인
+            db.collection("meetings").document(meeting.id).getDocument { document, error in
+                if let error = error {
+                    print("모임 참여 여부 확인 중 오류 발생: \(error)")
+                    return
+                }
+                
+                if let document = document, document.exists {
+                    let meetingMembers = document.data()?["meetingMembers"] as? [String] ?? []
+                    
+                    if meetingMembers.contains(friendID) {
+                        // 이미 모임의 멤버인 경우 알림
+                        alertMessage = "이미 해당 모임의 멤버인 친구입니다."
+                        showAlert = true
+                        completedRequests += 1 // 요청 완료 수 증가
                         return
                     }
-                    
-                    if let documents = snapshot?.documents, !documents.isEmpty {
-                        // 이미 요청이 있는 경우
-                        alertMessage = "모임 요청을 이미 보낸 상태입니다."
-                        showAlert = true
-                    } else {
-                        // 요청이 없는 경우 새 요청 생성
-                        let invitationData: [String: Any] = [
-                            "fromUserID": currentUserID,
-                            "fromUserName": currentUserName,
-                            "toUserID": friendID,
-                            "meetingName": meeting.title,
-                            "status": "pending",
-                            "meetingID": meeting.id
-                        ]
-                        db.collection("meetingRequests").addDocument(data: invitationData) { error in
-                            if let error = error {
-                                print("모임 초대 요청 전송 실패: \(error)")
-                                failedInvitations.append(friendID)
-                            } else {
-                                print("\(friendID)에게 모임 초대 요청 성공적으로 전송")
-                            }
-                            
-                            completedRequests += 1 // 요청 완료 수 증가
-                            
-                            // 모든 요청이 완료된 후 알림 메시지 설정
-                            if completedRequests == totalFriendsCount {
-                                if failedInvitations.isEmpty {
-                                    alertMessage = "모임 초대 요청이 성공적으로 전송되었습니다!"
+                }
+                
+                // 요청이 없는 경우 새 요청 생성
+                db.collection("meetingRequests")
+                    .whereField("fromUserID", isEqualTo: currentUserID)
+                    .whereField("toUserID", isEqualTo: friendID)
+                    .whereField("meetingName", isEqualTo: meeting.title)
+                    .getDocuments { snapshot, error in
+                        if let error = error {
+                            print("요청 확인 중 오류 발생: \(error)")
+                            return
+                        }
+                        
+                        if let documents = snapshot?.documents, !documents.isEmpty {
+                            // 이미 요청이 있는 경우
+                            alertMessage = "모임 요청을 이미 보낸 상태입니다."
+                            showAlert = true
+                        } else {
+                            // 요청이 없는 경우 새 요청 생성
+                            let invitationData: [String: Any] = [
+                                "fromUserID": currentUserID,
+                                "fromUserName": currentUserName,
+                                "toUserID": friendID,
+                                "meetingName": meeting.title,
+                                "status": "pending",
+                                "meetingID": meeting.id
+                            ]
+                            db.collection("meetingRequests").addDocument(data: invitationData) { error in
+                                if let error = error {
+                                    print("모임 초대 요청 전송 실패: \(error)")
+                                    failedInvitations.append(friendID)
                                 } else {
-                                    alertMessage = "\(failedInvitations.count)명에게 초대 요청 전송에 실패했습니다."
+                                    print("\(friendID)에게 모임 초대 요청 성공적으로 전송")
                                 }
-                                showAlert = true
+                                
+                                completedRequests += 1 // 요청 완료 수 증가
+                                
+                                // 모든 요청이 완료된 후 알림 메시지 설정
+                                if completedRequests == totalFriendsCount {
+                                    if failedInvitations.isEmpty {
+                                        alertMessage = "모임 초대 요청이 성공적으로 전송되었습니다!"
+                                    } else {
+                                        alertMessage = "\(failedInvitations.count)명에게 초대 요청 전송에 실패했습니다."
+                                    }
+                                    showAlert = true
+                                }
                             }
                         }
                     }
-                }
+            }
         }
     }
-}
-
-struct FriendRow: View {
-    var friend: FriendModel
-    var isSelected: Bool
-    var onTap: () -> Void
     
-    var body: some View {
-        VStack(alignment: .leading) {
-            Text(friend.name).font(.headline)
-            Text(friend.email).font(.subheadline)
-            Text("Phone: \(friend.phoneNumber)")
-            Text("Birthday: \(friend.birthday)")
-        }
-        .padding()
-        .background(Color.white)
-        .overlay(
-            RoundedRectangle(cornerRadius: 10)
-                .stroke(isSelected ? Color.blue : Color.clear, lineWidth: 2)
-        )
-        .onTapGesture {
-            onTap()
+    struct FriendRow: View {
+        var friend: FriendModel
+        var isSelected: Bool
+        var onTap: () -> Void
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                Text(friend.name).font(.headline)
+                Text(friend.email).font(.subheadline)
+                Text("Phone: \(friend.phoneNumber)")
+                Text("Birthday: \(friend.birthday)")
+            }
+            .padding()
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(isSelected ? Color.blue : Color.clear, lineWidth: 2)
+            )
+            .onTapGesture {
+                onTap()
+            }
         }
     }
 }
