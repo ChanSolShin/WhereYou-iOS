cb329d3a18afa621ccfbc1b07fc9a63b252e002c
= <leeh4743@naver.com>
2025-02-18 01:11:25 +0900

    Feat. AppDelegate
    Firebase Messaging 을 위한 FCM 토큰을 받아오는 AppDelegate 파일 생성 및 토큰 받아오는 코드 작성

--- iOS_Project/iOS_ProjectApp.swift
+++ iOS_Project/iOS_ProjectApp.swift
@@ -10,16 +10,19 @@ import Firebase
 import CoreLocation
 import NMapsMap
 import UserNotifications
+import FirebaseMessaging
 
 @main
 struct iOS_ProjectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     @ObservedObject private var locationCoordinator = AppLocationCoordinator.shared
     @StateObject private var loginViewModel = LoginViewModel()
     @State private var showAlert = false
     @State private var showNotificationAlert = false // 알림 권한 요청 상태
     
     init() {
-        FirebaseApp.configure()
+        FirebaseApp.configure()  // Firebase 초기화
     }
     
     var body: some Scene {
@@ -30,7 +33,6 @@ struct iOS_ProjectApp: App {
                         MainTabView()
                             .onAppear {
                                 locationCoordinator.startUpdatingLocation()
-                                // 로그인 후 강제 로그아웃 리스너는 LoginViewModel에서 처리됨.
                             }
                     } else {
                         LoginView()
@@ -44,7 +46,6 @@ struct iOS_ProjectApp: App {
                     PermissionRequiredView() // 설정으로 이동하는 화면
                 }
             }
-            .environmentObject(loginViewModel) // LoginViewModel을 전역에서 사용
             .onAppear {
                 // 위치 권한이 허용되지 않으면 경고 표시
                 if locationCoordinator.authorizationStatus != .authorizedAlways {
@@ -58,16 +59,6 @@ struct iOS_ProjectApp: App {
                 if status == .denied || status == .restricted {
                     showAlert = true
                 }
-            }
-            // 강제 로그아웃 발생 시 알림 표시 (로그인된 상태에서만 적용됨)
-            .alert(isPresented: $loginViewModel.forcedLogout) {
-                Alert(
-                    title: Text("강제 로그아웃"),
-                    message: Text("다른 기기에서 로그인되어 로그아웃되었습니다."),
-                    dismissButton: .default(Text("확인"), action: {
-                        loginViewModel.forcedLogout = false
-                    })
-                )
             }
             .alert(isPresented: $showAlert) {
                 Alert(
