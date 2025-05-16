6828840b8f423494e768043b818bc0e76821dfe7
= <leeh4743@naver.com>
2025-05-16 22:56:22 +0900

    Fix. MeetingViewMap
    위치 추적 중일때, Map에 위치가 실시간으로 렌더링 되지 않고, 위치추적 중지 했다가 다시 위치추적을 해야 Map이 업데이트 되는 현상 수정

--- iOS_Project/Profile/ProfileView.swift
+++ iOS_Project/Profile/ProfileView.swift
@@ -100,7 +100,7 @@ struct ProfileView: View {
                         Text("버전 정보")
                             .font(.body)
                         Spacer()
-                        Text("1.0.1v")
+                        Text("1.0.2v")
                             .foregroundColor(.gray)
                     }
                     .padding(.vertical, 8)
