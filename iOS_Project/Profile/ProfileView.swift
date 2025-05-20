4c8f19e5d0dc285bf2c7e905d7dc6864e7f61f07
= <leeh4743@naver.com>
2025-05-20 23:29:57 +0900

    Fix. Error
    인증토큰 만료 오류 수정

--- iOS_Project/Profile/ProfileView.swift
+++ iOS_Project/Profile/ProfileView.swift
@@ -100,7 +100,7 @@ struct ProfileView: View {
                         Text("버전 정보")
                             .font(.body)
                         Spacer()
-                        Text("1.0.2v")
+                        Text("1.0.3v")
                             .foregroundColor(.gray)
                     }
                     .padding(.vertical, 8)
