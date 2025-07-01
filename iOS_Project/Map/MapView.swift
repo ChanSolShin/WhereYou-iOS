a6c71056ca681722f02eaf90bbe8e978c59c4098
= <leeh4743@naver.com>
2025-07-01 21:53:03 +0900

    Feat. SearchLocation
    주소검색기능 구현

--- iOS_Project/Map/MapView.swift
+++ iOS_Project/Map/MapView.swift
@@ -13,18 +13,20 @@ import CoreLocation
 struct MapView: View {
     var isMarkerEnabled: Bool // 마커 추가 기능을 활성화하는 변수
     @ObservedObject var viewModel: AddMeetingViewModel // ViewModel 추가
+    @Binding var selectedLocation: CLLocationCoordinate2D?
 
     @StateObject private var coordinator: Coordinator
 
-    init(isMarkerEnabled: Bool, viewModel: AddMeetingViewModel) {
+    init(isMarkerEnabled: Bool, viewModel: AddMeetingViewModel, selectedLocation: Binding<CLLocationCoordinate2D?>) {
         self.isMarkerEnabled = isMarkerEnabled
         self.viewModel = viewModel
-        _coordinator = StateObject(wrappedValue: Coordinator(viewModel: viewModel)) // ViewModel 
+        self._selectedLocation = selectedLocation
+        self._coordinator = StateObject(wrappedValue: Coordinator(viewModel: viewModel, selectedLocation: selectedLocation))
     }
 
     var body: some View {
         VStack {
-            NaverMap(coordinator: coordinator, isMarkerEnabled: isMarkerEnabled, viewModel: viewModel)
+            NaverMap(coordinator: coordinator, isMarkerEnabled: isMarkerEnabled, viewModel: viewModel, selectedLocation: $selectedLocation)
         }
     }
 }
@@ -33,6 +35,7 @@ struct NaverMap: UIViewRepresentable {
     @ObservedObject var coordinator: Coordinator
     var isMarkerEnabled: Bool
     @ObservedObject var viewModel: AddMeetingViewModel // ViewModel 추가
+    @Binding var selectedLocation: CLLocationCoordinate2D?
 
     func makeCoordinator() -> Coordinator {
         coordinator
@@ -44,15 +47,14 @@ struct NaverMap: UIViewRepresentable {
         naverMapView.mapView.touchDelegate = context.coordinator
         naverMapView.mapView.addCameraDelegate(delegate: context.coordinator)
         naverMapView.mapView.addOptionDelegate(delegate: context.coordinator)
-
-        if isMarkerEnabled {
-            naverMapView.mapView.touchDelegate = context.coordinator
-        } else {
-            naverMapView.mapView.touchDelegate = nil
-        }
-
         return naverMapView
     }
 
-    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {}
+    func updateUIView(_ uiView: NMFNaverMapView, context: Context) {
+        if let location = selectedLocation {
+            if coordinator.currentMarker == nil || coordinator.currentMarker?.position.lat != location.latitude || coordinator.currentMarker?.position.lng != location.longitude {
+                coordinator.addMarkerAndMove(to: location, with: viewModel.meeting.meetingAddress ?? "선택된 장소")
+            }
+        }
+    }
 }
